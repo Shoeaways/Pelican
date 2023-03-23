72c99d7f4a40df5fb671f4a2b45c0eef27495e96
Eric Shu <eshu2146@gmail.com>
2023-03-23 12:49:41 -0700

    New Beginnings

---
+++ V1.0 Pelican/V1.0 Pelican.ino
@@ -0,0 +1,20 @@
+/*
+    Main code for the Pelican Drone
+
+    Rev 1.0 Changes
+    -
+
+    TODO:
+    -
+
+*/
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    Serial.print("There is no code yet");
+
+    delay(4000);
+}
